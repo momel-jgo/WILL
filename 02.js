81924374d51c8fe1155f730b7005f0e03c041216
Jenny Buitrago <melliebuitrago@gmail.com>
2022-11-09 16:04:12 -0500

    second commit

--- 02.js
+++ 02.js
@@ -12,6 +12,7 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+           return strings.sort((a,b)=> a.length - b.length).pop();
 }
 
 // No modifiques nada debajo de esta linea //
